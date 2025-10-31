b2b1da0b244a91626bd68e9ef1795dbeb3010134
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-10-30 19:34:43 -0600

    Prep 2.0

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Input a grade percentage: ");
+        string percent;
+        percent = Console.ReadLine();
+        float intPercent = Convert.ToInt32(percent);
+        string letter = "";
+        if (intPercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (intPercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (intPercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (intPercent >= 60)
+        {
+            letter = "D";
+        }
+        else if (intPercent < 60)
+        {
+            letter = "D";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (intPercent >= 70)
+        {
+            Console.WriteLine("You pass!");
+        }
     }
 }
