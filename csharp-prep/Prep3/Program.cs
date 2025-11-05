8678f758112713177e0f07f907f92cfa6eadf09a
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-04 19:28:16 -0700

    Prep 3 Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,8 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
-        Console.Write("Input the magic number: ");
-        int magicNumber = Convert.ToInt16(Console.ReadLine());
-        Console.Write(magicNumber);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        // Here to test if everything is working
+        // Console.WriteLine(magicNumber);
+        int guessNumber = 0;
+        int guessCount = 0;
+        do
+        {
+            Console.Write("Guess a number between 1 and 100: ");
+            guessNumber = Convert.ToInt16(Console.ReadLine());
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Guess lower!");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Guess higher!");
+            }
+            guessCount++;
+        } while (guessNumber != magicNumber);
+        Console.WriteLine("That's the one!");
+        Console.WriteLine($"It took you {guessCount} guesses.");
     }
 }
