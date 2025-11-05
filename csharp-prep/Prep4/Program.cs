bdfe91240aee770e66bdc9ef642ff18994e66bb2
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-04 19:47:27 -0700

    Prep 4 makes a list properly

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        
+        int newNumber = 0;
+        do
+        {
+            Console.Write("Enter a number: ");
+            newNumber = Convert.ToInt32(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        } while (newNumber != 0);
+        foreach (int number in numbers)
+        {
+        Console.WriteLine(number);
+        }
+        
+        
     }
 }
