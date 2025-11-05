399825d31e83b5b86c39139c0b70f7e4508e6b7b
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-04 19:10:01 -0700

    Initial work for Solar system Set up the input for prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Input the magic number: ");
+        int magicNumber = Convert.ToInt16(Console.ReadLine());
+        Console.Write(magicNumber);
     }
 }
--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,20 @@
 {
     static void Main()
     {
-        Console.WriteLine("Bonjour tout le monde");
+        // Console.WriteLine("Bonjour tout le monde");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 3031.9;
+        mercury.DisplayPlanetInformation();
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 7520.8;
+        venus.DisplayPlanetInformation();
+
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem._solarSystem.Add(mercury);
+        solarSystem._solarSystem.Add(venus);
+
     }
 }
