399bfe71b4cb7dde99cfbf931dc07fe63b518aaf
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-04 20:18:25 -0700

    Prep 5 DisplayWelcome and PromptUserName both work

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Hello Prep5 World!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        DisplayWelcome();
+        Console.WriteLine(PromptUserName());
+        
+        
     }
 }
