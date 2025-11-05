7653210a19dfd49f01c1a2c4f9d22f83a5154a8c
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-04 20:26:54 -0700

    Prep 4 PromptUserNumber and PromptUserBirthYear functional

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -14,9 +14,24 @@ class Program
             string name = Console.ReadLine();
             return name;
         }
-
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = Convert.ToInt16(Console.ReadLine());
+            return number;
+        }
+        static void PromptUserBirthYear(out int year)
+        {
+            Console.Write("Please enter your birth year: ");
+            year = Convert.ToInt16(Console.ReadLine());
+            
+        }
         DisplayWelcome();
-        Console.WriteLine(PromptUserName());
+        // Console.WriteLine(PromptUserName());
+        // Console.WriteLine(PromptUserNumber());
+        int year;
+        PromptUserBirthYear(out year);
+        Console.WriteLine(year);
         
         
     }
