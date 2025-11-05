ccdcbc6a4496e6c12ed9a9c764aacfd4ca3cdf7a
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-04 20:12:17 -0700

    Prep 4 functional with the extra part done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,9 +6,11 @@ class Program
     {
         Console.WriteLine("Hello Prep4 World!");
         List<int> numbers = new List<int>();
-        
-        
-        int newNumber = 0;
+
+
+        int newNumber;
+        int largestNumber = 0;
+        int smallestPositive = 0;
         do
         {
             Console.Write("Enter a number: ");
@@ -16,12 +18,37 @@ class Program
             if (newNumber != 0)
             {
                 numbers.Add(newNumber);
+                if (newNumber > largestNumber)
+                {
+                    largestNumber = newNumber;
+                }
+                foreach (int number in numbers)
+                {
+                    if (newNumber <= number && newNumber > 0)
+                    {
+                        if (newNumber <= smallestPositive || smallestPositive == 0)
+                        {
+                            smallestPositive = newNumber;
+                        }
+                            
+                    }   
+                }
+                
             }
         } while (newNumber != 0);
+
+        float total = 0;
+
         foreach (int number in numbers)
         {
-        Console.WriteLine(number);
+            total += number;
         }
+        float average = total / numbers.Count;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        
         
         
     }
