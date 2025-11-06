fff070cb3a9eb67579adacbc449bec7b9f9a50cf
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-06 09:51:09 -0700

    Menu Class completed, we hope

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,8 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Menu journalMenu = new Menu();
+        int userSelection;
+        userSelection = journalMenu.ProcessMenu();
 
-        journalMenu.ProcessMenu();
+        switch(userSelection)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
     }
 }
 
