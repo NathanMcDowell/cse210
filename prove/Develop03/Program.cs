df1844222a6d9dbe7a708dfc5c96416f78f67a30
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-13 09:21:12 -0700

    Initial word initialization

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        Word myWord = new Word("Dude");
+        myWord.DisplayWord();
     }
 }
