e334234f7fdd088cd32df0be5bb07912527fb947
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-18 09:04:04 -0700

    Person program, Person.cs, and Police.cs initialization

---
+++ sandbox/PersonProject/Person.cs
@@ -0,0 +1,42 @@
+class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+    private int _weight;
+
+    public Person(string firstName, string lastName, int age, int weight)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        SetAge(age);
+        SetWeight(weight);
+    }
+    public string PersonInformation()
+    {
+        return $"{_firstName} {_lastName}, {_age} years old and {_weight} pounds";
+    }
+    public void SetAge(int age)
+    {
+        if (age > 0)
+        {
+            _age = age;
+        }
+        else
+        {
+            _age = 0;
+        }
+    }
+    public void SetWeight(int weight)
+    {
+        if (weight > 0)
+        {
+            _weight = weight;
+        }
+        else
+        {
+            _weight = 0;
+        }
+        
+    }
+}
---
+++ sandbox/PersonProject/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Person person = new("John", "Smith", 99, 220);
+        Console.WriteLine(person.PersonInformation());
+
+        Police police = new Police("Taser and Jujitsu","Barbara", "Gordon", 28, 150);
+        Console.WriteLine(police.PoliceInformation());
+    }
+}
