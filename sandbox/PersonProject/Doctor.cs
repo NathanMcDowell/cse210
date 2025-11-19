668773272670eda534a737ecb73495c7a57674ad
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-18 17:20:11 -0700

    More class work with Person Program

---
+++ sandbox/PersonProject/Doctor.cs
@@ -0,0 +1,14 @@
+class Doctor : Person
+{
+    private string _tools;
+
+    public Doctor(string tools, string firstName, string lastName, int age, int weight)
+    : base (firstName, lastName, age, weight)
+    {
+        _tools = tools;
+    }
+    public string DoctorInformation()
+    {
+        return $"{PersonInformation()}, Tools: {_tools}";
+    }
+}
--- sandbox/PersonProject/Program.cs
+++ sandbox/PersonProject/Program.cs
@@ -8,5 +8,14 @@
 
         Police police = new Police("Taser and Jujitsu","Barbara", "Gordon", 28, 150);
         Console.WriteLine(police.PoliceInformation());
+
+        Doctor doctor = new("Leeches", "Plague", "Doctor", 50, 140);
+        Console.WriteLine(doctor.DoctorInformation());
+        // doctor.SetAge(51);
+        // doctor.SetWeight(150);
+        // Console.WriteLine(doctor.DoctorInformation());
+
+        Surgeon surgeon = new("Doctor", "Bandages", "John", "Detective", 30, 162);
+        Console.WriteLine(surgeon.SurgeonInformation());
     }
 }
