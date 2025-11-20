167db813ee7a5dce23fbbc2b8d7edc67effeabba
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-20 09:45:35 -0700

    Activity is initialized and two display functions work

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        Activity activity = new("Super Duper", "A really cool activity");
+
+        int response = 0;
+        while (response != 4)
+        {
+            Console.WriteLine("1. Breathing");
+            Console.WriteLine("2. Reflection");
+            Console.WriteLine("3. Listing");
+            Console.WriteLine("4. Quit");
+
+            response = int.Parse(Console.ReadLine());
+            
+            if (response == 1)
+            {
+               activity.DisplayGreeting();
+               activity.DisplayDescription();
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,30 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        string animationString = "\\|/-";
+        string animationString = ".-oO~*~Oo-";
         int sleepTime = 250;
-        int duration = 20;
+        int duration = 10;
         int index = 0;
         DateTime currentTime = DateTime.Now;
         DateTime endTime = currentTime.AddSeconds(duration);
-
-        // while(DateTime.Now < endTime)
-        // {
-        //     Console.Write(animationString[index++ % animationString.Length]);
-        //     Thread.Sleep(sleepTime);
-        //     Console.Write("\b");
-        // }
-
-        int count = duration;
+        
+        Console.CursorVisible = false;
         while(DateTime.Now < endTime)
         {
-            Console.Write(count--);
-            Thread.Sleep(1000);
-            if (count >= 9)
-                Console.Write("\b\b  \b\b");
-            else
-                Console.Write("\b");
+            Console.Write(animationString[index++ % animationString.Length]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
         }
+        Console.CursorVisible = true;
+        // int count = duration;
+        // while(DateTime.Now < endTime)
+        // {
+        //     Console.Write(count--);
+        //     Thread.Sleep(1000);
+        //     if (count >= 9)
+        //         Console.Write("\b\b  \b\b");
+        //     else
+        //         Console.Write("\b");
+        // }
         
     }
 }
