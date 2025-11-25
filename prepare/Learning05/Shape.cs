dda18a2a8334998591906ef233a94372806b0edb
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-11-25 15:57:50 -0700

    All the files are made, and the skeletons of the Shape class is made

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,14 @@
+class Shape
+{
+    private string _color;
+
+    public string GetColor()
+    {
+        
+    }
+    public void SetColor(string color)
+    {
+        
+    }
+    virtual public double GetArea();
+}
