85410d6d68c9069323d3f87a033d8f342091bc39
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2025-12-13 13:35:18 -0700

    Wizard constructor works

--- final/FinalProject/Wizard.cs
+++ final/FinalProject/Wizard.cs
@@ -2,6 +2,15 @@ class Wizard : SpellCaster
 {
     public Wizard() : base()
     {
-        
+        int dexMod = GetAbilityMods()[1];
+        SetArmorType("None");
+        SetArmorClass(10 + dexMod);
+    }
+    public Wizard(string name, string characterClass, int maxHealth, List<int> abilityScores, string focus) 
+    : base(name, characterClass, maxHealth, abilityScores, focus, false)
+    {
+        int dexMod = GetAbilityMods()[1];
+        SetArmorType("None");
+        SetArmorClass(10 + dexMod);
     }
 }
